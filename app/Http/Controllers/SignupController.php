fecc5adf199aeb4460c2b56415f0d2caca17e208
Sagar Singh <sagar966028@gmail.com>
2022-07-25 21:31:07 +0530

    Inital commit

---
+++ app/Http/Controllers/SignupController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Signup;
+use Illuminate\Http\Request;
+
+class SignupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $res=new signup;
+        $res->name=$request->input('name');
+        $res->email=$request->input('email');
+        $res->city=$request->input('city');
+        $res->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Signup  $signup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Signup $signup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Signup  $signup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Signup $signup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Signup  $signup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Signup $signup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Signup  $signup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Signup $signup)
+    {
+        //
+    }
+}
